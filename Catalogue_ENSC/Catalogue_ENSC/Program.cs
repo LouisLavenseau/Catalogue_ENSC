799d99dd21ec97b5c280a77b08b4fd57857554b0
louloulala <llavenseau@ensc.fr>
2020-05-09 18:42:40 +0200

    test console.writeline

---
+++ Catalogue_ENSC/Catalogue_ENSC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogue_ENSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("POO");
+        }
+    }
+}
